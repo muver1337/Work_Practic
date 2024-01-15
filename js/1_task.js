cd022d7040908b1d5b82f13b12fa8c89722f663d
muver1337 <muvers1s@gmail.com>
2024-01-15 18:05:34 +0700

    Commit

---
+++ js/1_task.js
@@ -0,0 +1,18 @@
+// Вывести все уникальные символы из строки 'DDADSADASDAAADS'
+let findUnique = 'DDADSADASDAAADS';
+
+// Создаём Set для хранения уникальных значений
+let unique = new Set();
+
+// Переборка строки по символам
+for (let char of findUnique){
+    if (!unique.has(char)){
+        unique.add(char);
+    }
+}
+
+// Преобразовываем массив уникальных значений в строку
+let uniqueString = Array.from(unique).join('');
+
+// Вывод в консоль
+console.log("Task 1 :", uniqueString);
