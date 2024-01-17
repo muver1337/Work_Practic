17430b88a4f92e3cf80d37321e73b8291907234d
muver1337 <muvers1s@gmail.com>
2024-01-17 23:09:53 +0700

    Commit

---
+++ js/12_task.js
@@ -0,0 +1,17 @@
+class Worker {
+    constructor(name, surname, rate, days) {
+        this.name = name;
+        this.surname = surname;
+        this.rate = rate;
+        this.days = days;
+    }
+
+    getSalary() {
+        return this.rate * this.days;
+    }
+}
+
+let worker = new Worker("Михаил", "Михаилов", 1600, 31);
+
+console.log("Task 12:", worker)
+console.log("Task 12: Зарплата -", worker.getSalary())
